aee2c8032cb27556e40539bda53f8eac29512abf
Generatorman <generatorman.ai@gmail.com>
2023-03-07 06:01:10 +0530

    add src to import diffusers in colab

--- src/diffusers/schedulers/scheduling_ddim_inverse.py
+++ src/diffusers/schedulers/scheduling_ddim_inverse.py
@@ -21,9 +21,9 @@ from typing import List, Optional, Tuple, Union
 import numpy as np
 import torch
 
-from diffusers.configuration_utils import ConfigMixin, register_to_config
-from diffusers.schedulers.scheduling_utils import SchedulerMixin
-from diffusers.utils import BaseOutput
+from src.diffusers.configuration_utils import ConfigMixin, register_to_config
+from src.diffusers.schedulers.scheduling_utils import SchedulerMixin
+from src.diffusers.utils import BaseOutput
 
 
 @dataclass
